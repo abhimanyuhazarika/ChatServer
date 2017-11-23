4b80c5832dbd65185f6580f4f0febac3650156e5
abhimanyuhazarika <32271162+abhimanyuhazarika@users.noreply.github.com>
2017-11-23 19:47:47 +0000

    Update ChatHelpers.hs

--- src/ChatHelpers.hs
+++ src/ChatHelpers.hs
@@ -1,1 +1,13 @@
-
+data Client    = Client { clientName :: String
+                        , subs       :: HashTable Int (Chan String)
+                        , joinId     :: Int
+                        }
+data Clients   = Clients { lastClientId   :: Int
+                         , theClients     :: HashTable Int Client
+                         , clientsNames   :: HashTable String Int
+                         }
+data ChatRoom  = ChatRoom Int (Chan String)
+data ChatRooms = ChatRooms { chatRoomFromId     :: HashTable Int ChatRoom
+                           , chatRoomIdFromName :: HashTable String Int
+                           , numberOfChatRooms  :: Int  
+                           }
