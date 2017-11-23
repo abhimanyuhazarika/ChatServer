e208f973e3febba3534e25418f9a0074fbe40ff4
abhimanyuhazarika <32271162+abhimanyuhazarika@users.noreply.github.com>
2017-11-23 22:31:34 +0000

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -20,23 +20,50 @@ main = do
 server :: Socket -> String -> Chan Bool -> MVar Clients -> MVar ChatRooms -> IO ()
 server sock port killedChan clients chatrooms = do
     clog "Waiting for incoming connection..."
-    conn <- try (accept sock) :: IO (Either SomeException (Socket, SockAddr))  -- try to accept a connection and handle it
+    conn <- try (accept sock) :: IO (Either SomeException (Socket, SockAddr)) 
     case conn of
         Left  _    -> clog "Socket is now closed. Exiting."
         Right conn -> do
             clog "Got a client !"
-            runClient conn sock port killedChan clients chatrooms       -- run our client's logic, then
-            server sock port killedChan clients chatrooms               -- repeat
+            runClient conn sock port killedChan clients chatrooms       
+            server sock port killedChan clients chatrooms               
 
+loopClient :: Handle -> Socket -> String -> Chan Bool -> MVar Clients -> MVar ChatRooms -> [Int] -> IO ()
+loopClient hdl originalSocket port killedChan clients chatrooms joinIds = do
+    (kill, timedOut, input) <- waitForInput hdl killedChan 0 joinIds clients
+    when (timedOut) (clog "Client timed out")
+    when (kill || timedOut) (return ())
+    let commandAndArgs = splitOn " " input
+    let command = head commandAndArgs
+    let args = intercalate " " $ tail commandAndArgs
+    case command of
+        "HELO"            -> do
+            helo hdl args port
+            loopClient hdl originalSocket port killedChan clients chatrooms joinIds
+        "JOIN_CHATROOM:"  -> do
+            (error, id) <- join hdl args port clients chatrooms
+            if error then return ()
+            else do
+                if id `elem` joinIds then loopClient hdl originalSocket port killedChan clients chatrooms joinIds
+                else loopClient hdl originalSocket port killedChan clients chatrooms (id:joinIds)
+        "LEAVE_CHATROOM:" -> do
+            (error, id) <- leave hdl args clients
+            if error then return ()
+            else do
+                loopClient hdl originalSocket port killedChan clients chatrooms (delete id joinIds)
+        "DISCONNECT:"     -> do
+            error <- disconnect hdl args clients
+            unless error (loopClient hdl originalSocket port killedChan clients chatrooms joinIds)
+        "CHAT:"           -> do
+            error <- chat hdl args clients
+            unless error (loopClient hdl originalSocket port killedChan clients chatrooms joinIds)
+        _                 -> otherCommand hdl input
 
-mainLoop :: Socket -> IO ()
-mainLoop sock = do
-    conn <- accept sock     -- accept a connection and handle it
-    runConn conn            -- run our server's logic
-    mainLoop sock           -- repeat
-runConn :: (Socket, SockAddr) -> IO ()
-runConn (sock, _) = do
+runClient :: (Socket, SockAddr) -> Socket -> String -> Chan Bool -> MVar Clients -> MVar ChatRooms -> IO ()
+runClient (sock, addr) originalSocket port killedChan clients chatrooms = do
     hdl <- socketToHandle sock ReadWriteMode
-    hSetBuffering hdl NoBuffering
-    hPutStrLn hdl "Hello!"
+    hSetBuffering hdl LineBuffering
+    handle (\(SomeException _) -> return ()) $ fix $ (\loop -> (loopClient hdl originalSocket port killedChan clients chatrooms []))
+    -- shutdown sock ShutdownBoth
     hClose hdl
+    clog "Client disconnected"
